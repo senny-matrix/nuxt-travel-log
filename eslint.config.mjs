d7c8660656c8cd814e1e6bab945dc0a612c4a9ec
Rogers Aaron <arumeru@gmail.com>
2025-05-28 20:10:11 +0300

    Integrated Tailwindcss, DaisyUI, Icons and implemented dark and light mode themes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import antfu from "@antfu/eslint-config";
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
+// TODO: Add tailwindcss plugin
+
 export default withNuxt(
   // Your custom configs here
   antfu({
