8d1c621541d9f63bd26ba996f9921277275cb608
Rogers Aaron <arumeru@gmail.com>
2025-05-28 21:15:54 +0300

    arumeru | feat: Adding Landing Page

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center  min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple
+          travel log App. Add locations, photos, and notes to create
+          a digital jounal of your jorneys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In with GitHub
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
