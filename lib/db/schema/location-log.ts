a5283a3d14842f9a7785130c15088d9d4546eaae
Rogers Aaron <arumeru@gmail.com>
2025-05-30 09:19:32 +0300

    :arumeru | added db setup

---
+++ lib/db/schema/location-log.ts
@@ -0,0 +1,16 @@
+import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { location } from "./location";
+
+export const locationLog = sqliteTable("locationLog", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  description: text(),
+  startedAt: int().notNull(),
+  endedAt: int().notNull(),
+  locationId: int().notNull().references(() => location.id),
+  lat: real().notNull(),
+  long: real().notNull(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
